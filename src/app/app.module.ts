c70765ebde7f87095b495ed271dc695292b5ee1e
mathieubourmaud <mathieu.bourmaud@gmail.com>
2017-04-12 23:30:07 +0200

    Front end is done. Now the nodeJS part :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { MdIconModule, MdSnackBarModule, MdInputModule, MdButtonModule, MdCheckboxModule, MdCardModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MdToolbarModule} from '@angular/material';
+
+
+
+const appRoutes: Routes = [
+  { path: 'todos', component: TodosComponent },
+  { path: 'todos/:id', component: TodoComponent },
+  { path: '',   redirectTo: '/todos', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule.forRoot(appRoutes),
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdInputModule,
+    MdIconModule,
+    MdSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
